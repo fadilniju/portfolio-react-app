d13d5c62210f69f53c4d5788e3a32e578308eacb
Fadil p <76585854+fadilniju@users.noreply.github.com>
2025-07-18 15:13:50 +0530

    Delete components directory

--- components/Navbar.tsx
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-const Navbar = () => { return <nav className="h-20"></nav>; };
-export default Navbar;
--- components/Skills.tsx
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-const Skills = () => { return <section id="skills" className="h-screen"></section>; };
-export default Skills;
